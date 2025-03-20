4035967f1073fd3595d200ffe3e9a4f17984c7cb
n428907 <n428907@scts.santander.de>
2025-03-20 16:34:17 +0100

    skills vorbereitet

--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -1,17 +1,12 @@
-// myComponent.js
-const skillsContainerStyle = {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100vh",
-    position: "relative"
-};
+// src/pages/skills.jsx
+import React, {useState} from "react";
+import "../assets/bubble_style.css";
+import skills from "../data/skills.json";
+
 
 const skillBubbleStyle = {
     margin: "10px",
     borderRadius: "50%",
-    boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
     cursor: "pointer",
     display: "inline-flex",
     justifyContent: "center",
@@ -20,20 +15,6 @@ const skillBubbleStyle = {
     position: "absolute"
 };
 
-// Funktion zum Umwandeln von Hex in RGB
-function hexToRgb(hex) {
-    // Entferne das '#' am Anfang, falls vorhanden
-    hex = hex.replace(/^#/, '');
-    // Teile die Farbe in ihre RGB-Komponenten auf
-    const bigint = parseInt(hex, 16);
-    const r = (bigint >> 16) & 255;
-    const g = (bigint >> 8) & 255;
-    const b = bigint & 255;
-
-    return {r, g, b};
-}
-
-// Funktion zum Anpassen der Helligkeit
 function adjustBrightness(color, factor) {
     return {
         r: Math.min(255, Math.max(0, Math.floor(color.r * factor))),
@@ -42,63 +23,66 @@ function adjustBrightness(color, factor) {
     };
 }
 
-// Funktion zum Umwandeln von RGB in Hex
-function rgbToHex(r, g, b) {
-    return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase()}`;
+function rgbToCss(rgb, transparency = 1) {
+    return `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, ${transparency})`;
 }
 
 function getRandomPosition() {
-    const x = Math.random() * 80 + 10; // 10% bis 90% der Breite
-    const y = Math.random() * 80 + 10; // 10% bis 90% der Höhe
+    const range = 75;
+    const x = Math.random() * range;
+    const y = Math.random() * range;
     return {x, y};
 }
 
 function SkillBubble({skill}) {
-    const {x, y} = getRandomPosition();
-    const rgbColor = hexToRgb(skill.color);
+    const [position, setPosition] = useState(getRandomPosition());
+    const [size, setSize] = useState(skill.size);
+    const [animate, setAnimate] = useState(false);
+    const [pop, setPop] = useState(false);
+
+    const handleClick = () => {
+        setPop(true);
+        setTimeout(() => {
+            setPosition(getRandomPosition());
+            setSize(0);
+            setAnimate(true);
+            setPop(false);
+            setTimeout(() => {
+                setSize(skill.size);
+                setAnimate(false);
+            }, 0);
+        }, 300); // Wartezeit für die Pop-Animation
+    };
+
+    const rgbColor = skill.rgb;
     const darker_factor = 0.95;
     const light_factor = 1.3;
-    const lightColor = rgbToHex(adjustBrightness(rgbColor, light_factor).r, adjustBrightness(rgbColor, light_factor).g, adjustBrightness(rgbColor, light_factor).b);
-    const darkColor = rgbToHex(adjustBrightness(rgbColor, darker_factor).r, adjustBrightness(rgbColor, darker_factor).g, adjustBrightness(rgbColor, darker_factor).b);
-
-    const realSize = skill.size
+    const lightColor = rgbToCss(adjustBrightness(rgbColor, light_factor));
+    const darkColor = rgbToCss(adjustBrightness(rgbColor, darker_factor));
 
     const dynamicStyle = {
         ...skillBubbleStyle,
-        width: `${realSize}vw`,
-        height: `${realSize}vw`,
+        width: `${size}vw`,
+        height: `${size}vw`,
         background: `linear-gradient(135deg, ${lightColor}, ${darkColor})`,
-        left: `${x}%`,
-        top: `${y}%`
+        boxShadow: `inset 10px 5px 5px ${rgbToCss(adjustBrightness(rgbColor, light_factor + 0.2), 0.5)}, 10px 5px 5px ${rgbToCss(adjustBrightness(rgbColor, darker_factor), 0.5)}`,
+        left: `${position.x}%`,
+        top: `${position.y}%`,
+        '--bubble-size': `${skill.size}vw`
     };
 
     return (
-        <div className={"bubble"} style={dynamicStyle}>
+        <div className={`bubble ${animate ? 'grow' : ''} ${pop ? 'pop' : ''}`} style={dynamicStyle} onClick={handleClick}>
             {skill.name}
         </div>
     );
 }
 
-const skills = [
-    {name: "Python", size: 25, color: "#BAE1FF", order: 0},
-    {name: "Django", size: 10, color: "#FFBAE1", order: 1},
-    {name: "Flask", size: 5, color: "#BAFFBA", order: 3},
-    {name: "Tornado", size: 10, color: "#FFBABA", order: 4},
-    {name: "Java", size: 5, color: "#FFDFBA", order: 5},
-    {name: "JavaScript", size: 15, color: "#FFFFBA", order: 5},
-    {name: "React", size: 20, color: "#D4A5A5", order: 5},
-    {name: "Vue", size: 10, color: "#BAFFC9", order: 5},
-    {name: "Angular", size: 5, color: "#FFB3BA", order: 5},
-    {name: "Node.js", size: 10, color: "#A5D4A5", order: 5},
-    {name: "Express", size: 5, color: "#A5A5D4", order: 5},
-    {name: "Spring", size: 10, color: "#D4A5D4", order: 5},
-];
-
 export default function Skills() {
-    return (<div style={skillsContainerStyle}>
-
+    return (
+        <div>
             <h1>Skills</h1>
             {skills.map((skill, index) => <SkillBubble skill={skill} key={index}/>)}
         </div>
-    )
+    );
 }

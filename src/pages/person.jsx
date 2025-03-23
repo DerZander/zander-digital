59c50490440a9e8d71a4e8ce0efe35c7a962e5f7
Timo Zander <timo.zander@outlook.com>
2025-03-23 18:42:08 +0100

    person started

--- src/pages/person.jsx
+++ src/pages/person.jsx
@@ -1,5 +1,40 @@
-export default function Person() {
+import {Avatar, Box, Typography} from "@mui/material";
+import meImage from "../assets/images/me.jpeg";
+
+function ShortPersonPage() {
     return (
-        <h1>Person</h1>
-    )
+        <Box
+            sx={{
+                width: "100%",
+                height: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#1e1e1e",
+                color: "#fff",
+                padding: 4,
+            }}
+        >
+            <Avatar
+                src={meImage}
+                sx={{width: 200, height: 200, border: "5px solid #fff", boxShadow: 4, mb: 3}}
+            />
+            <Typography variant="h3" gutterBottom>
+                Timo Zander
+            </Typography>
+            <Typography variant="h6" gutterBottom>
+                Softwareentwickler & Jugendwart
+            </Typography>
+            <Typography variant="body1" align="center" maxWidth="600px">
+                Leidenschaftlicher Fullstack-Entwickler mit Fokus auf React, Spring Boot und Datenanalyse.
+                Nebenberuflich Jugendwart bei der Feuerwehr und begeistert von DnD, 3D-Druck, Gaming und Teamarbeit.
+            </Typography>
+        </Box>
+    );
+}
+
+
+export default function PersonPage() {
+    return (<ShortPersonPage/>);
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -5,9 +5,8 @@ import ContactEmergencyIcon from "@mui/icons-material/ContactEmergency";
 import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
 import PsychologyIcon from '@mui/icons-material/Psychology';
 import * as React from "react";
-import Person from "./pages/person.jsx";
+import PersonPage from "./pages/person.jsx";
 import Projects from "./pages/projects.jsx";
-import SkillsBubbles from "./pages/skillsBubbles.jsx";
 import Gaming from "./pages/gaming.jsx";
 import Contact from "./pages/contact.jsx";
 import CV from "./pages/cv.jsx";
@@ -15,7 +14,7 @@ import SkillPage from "./pages/SkillPage.jsx";
 
 
 export const routes = [
-    {icon: <PersonIcon/>, name: 'Über Mich', id: 1, route: '/person', component: <Person/>},
+    {icon: <PersonIcon/>, name: 'Über Mich', id: 1, route: '/person', component: <PersonPage/>},
     {icon: <DashboardIcon/>, name: 'Projekte', id: 2, route: '/projects', component: <Projects/>}, //Programmierungsprojekte, Game Jam Projekte, Ehrenamtliche Projekte
     {icon: <PsychologyIcon/>, name: 'Skills', id: 3, route: '/skills', component: <SkillPage/>},
     {icon: <SportsEsportsIcon/>, name: 'Gaming-Bereich', id: 4, route: '/gaming', component: <Gaming/>},
